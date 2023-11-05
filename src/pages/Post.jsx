acad78cd5d834ca546da255766ef88406c200e93
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-06 01:21:49 +0530

    input conatiner added

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,25 +1,39 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
 // import Form from "../components/Form"
 
-export default function Post(){
-    return (
-      <main id="postItem">
-        <Navbar />
-        <section >
+export default function Post() {
+  return (
+    <main id="postItem">
+      <Navbar />
+      <section>
         <h1 className="lfh1">Post Found Item</h1>
         <div className="form-container">
           <h2>Please fill all the required fields</h2>
           <form action="">
-            <label htmlFor="">Name </label> <input type="text" />
-            <label htmlFor="">Email </label> <input type="email" />
-            <label htmlFor="">Phone </label> <input type="tel" />
-            <label htmlFor="">Title </label> <input type="Text" />
-            <label htmlFor="">Description </label> <textarea></textarea>
-            <input type="file" />
-            <input type="submit" />
+            <div className="input-container">
+              <label htmlFor="">Name </label> <input type="text" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="">Email </label> <input type="email" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="">Phone </label> <input type="tel" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="">Title </label> <input type="Text" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="">Description </label> <textarea></textarea>
+            </div>
+            <div className="input-container">
+              <input type="file" />
+            </div>
+            <div className="input-container">
+              <input type="submit" />
+            </div>
           </form>
         </div>
-        </section>
-      </main>
-    );
+      </section>
+    </main>
+  );
 }
