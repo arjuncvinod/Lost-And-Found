966a3ef3a9ecae05475d2a8dff5b501b6a3cfa9e
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-05 23:39:22 +0530

    form component created

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,10 +1,25 @@
 import Navbar from "../components/Navbar"
+import Form from "../components/Form"
 
 export default function Post(){
-    return(
-        <main>
-            <Navbar />
-        </main>
-
-    )
+    return (
+      <main>
+        <Navbar />
+        <section id="postItem">
+        <h1>Post Found Item</h1>
+        <div className="form-container">
+          <h1>Please fill all the required fields</h1>
+          <form action="">
+            <label htmlFor="">Name </label> <input type="text" />
+            <label htmlFor="">Email </label> <input type="email" />
+            <label htmlFor="">Phone </label> <input type="tel" />
+            <label htmlFor="">Title </label> <input type="Text" />
+            <label htmlFor="">Description </label> <textarea></textarea>
+            <input type="file" />
+            <input type="submit" />
+          </form>
+        </div>
+        </section>
+      </main>
+    );
 }
