f214c342ccb166fcb939731f1bbee640d10dcca0
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-05 23:20:13 +0530

    post item page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Find from "./pages/Find"
+import Post from "./pages/Post";
 import "./App.css"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />}  />
       <Route path='/find' element={<Find />}  />
+      <Route path='/post' element={<Post />}  />
     </Routes>
       )
 }
