f9002943bc47f37480308454a1b7e37b6a1163f7
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-06 19:40:41 +0530

    mongodb connected

---
+++ Backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = process.env.PORT || 3000;
+
+export const mongoURL ="mongodb+srv://root:SNzJheXm5ObQeQrh@lostandfound.toc33hc.mongodb.net/lost-items?retryWrites=true&w=majority";
