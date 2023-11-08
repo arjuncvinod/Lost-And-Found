8b31ccf0cd57bc49459565db17587ae2ccc558e1
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-08 14:11:14 +0530

    nav menu updated

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,14 +1,46 @@
-import logo from "../assets/logo.png"
-function Navbar(){
-return(
-<nav>
-    <img src={logo} alt="" />
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/find">Find item</a></li>
-        <li><a href="/post">Post item</a></li>
-    </ul>
-</nav>
-)
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+
+function Navbar() {
+  const [active, setActive] = useState(false);
+  const [cls,setCls]=useState("inactive")
+
+  function openNav() {
+    setActive(true)
+   setCls("active")
+    
+  }
+  function closeNav(){
+    setActive(false)
+    setCls("inactive")
+  }
+  return (
+    <nav>
+      <a href="/"><img src={logo} alt="" /></a>
+      {/* <ul style={{ width: `${width}` }}> */}
+      <ul className={cls}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/find">Find item</a>
+        </li>
+        <li>
+          <a href="/post">Post item</a>
+        </li>
+      </ul>
+      {active ? (
+        <button className="menu-container" onClick={closeNav}>
+          <CloseIcon className="menu close" />
+        </button>
+      ) : (
+        <button className="menu-container" onClick={openNav}>
+          <MenuIcon className="menu" />
+        </button>
+      )}
+    </nav>
+  );
 }
 export default Navbar;
