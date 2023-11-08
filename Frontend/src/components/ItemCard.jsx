660172fc5aa716f8921f9c5d09ded29798a09304
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-09 01:48:48 +0530

    added default image through state

--- Frontend/src/components/ItemCard.jsx
+++ Frontend/src/components/ItemCard.jsx
@@ -1,17 +1,32 @@
+import { useState ,useEffect} from "react";
 import { api } from "../config";
 import noImage from "../assets/no-image.png"
+import axios from "axios";
+
 
 export default function Itemcard(props) {
+  const [image, setImage] = useState(noImage);
+  useEffect(() => {
+    axios
+      .get(`${api}/files/${props.image}`)
+      .then((res) => {
+        setImage(`${api}/files/${props.image}`);
+      })
+      .catch((error) => {
+        setImage(noImage);
+      });
+
+
+  },[props.image]);
+
+
   return (
-    <a href={"/" + props.id} data-aos="fade-up">
+    <a href={"/find/details/" + props.id} data-aos="fade-up">
       <div className="card">
         <div className="card-img">
           <img
-            src={api+"/files/"+props.image}
+            src={image}
             alt=""
-            onError={(e) => {
-              e.target.src = noImage;
-            }}
           />
         </div>
         <div className="card-desc">
