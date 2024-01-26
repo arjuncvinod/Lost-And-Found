c294734f0b545dee6ba00675a00ae1a39b31d699
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-01-26 11:15:19 +0530

    api updated

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
 // export const api = "https://dull-gold-clam-tutu.cyclic.app"
 // export const api = "https://lostandfound-api.onrender.com";
 // export const api = "http://localhost:8000";
-export const api = "https://lost-and-found-api-nine.vercel.app/";
+export const api = "https://lost-and-found-api-nine.vercel.app";
